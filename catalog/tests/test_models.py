88cc6f8a93e8b70e18c15b7f2085bcb21b080712
marquant-matt <matt@marquant.io>
2020-07-15 15:16:43 +0100

    First version of application moved into github

---
+++ catalog/tests/test_models.py
@@ -0,0 +1,57 @@
+"""
+This file demonstrates writing tests using the unittest module. These will pass
+when you run "manage.py test".
+
+Replace this with more appropriate tests for your application.
+"""
+
+import django
+from django.test import TestCase
+django.setup()
+from catalog.models import Author
+
+class AuthorModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Author.objects.create(first_name='Big', last_name='Bob')
+
+    def test_first_name_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('first_name').verbose_name
+        self.assertEquals(field_label, 'first name')
+
+    def test_date_of_death_label(self):
+        author=Author.objects.get(id=1)
+        field_label = author._meta.get_field('date_of_death').verbose_name
+        self.assertEquals(field_label, 'died')
+
+    def test_first_name_max_length(self):
+        author = Author.objects.get(id=1)
+        max_length = author._meta.get_field('first_name').max_length
+        self.assertEquals(max_length, 100)
+
+    def test_object_name_is_last_name_comma_first_name(self):
+        author = Author.objects.get(id=1)
+        expected_object_name = f'{author.last_name}, {author.first_name}'
+        self.assertEquals(expected_object_name, str(author))
+
+    def test_get_absolute_url(self):
+        author = Author.objects.get(id=1)
+        # This will also fail if the urlconf is not defined.
+        self.assertEquals(author.get_absolute_url(), '/catalog/authors/1')
+
+#class SimpleTest(TestCase):
+#    """Tests for the application views."""
+
+#    # Django requires an explicit setup() when running tests in PTVS
+#    @classmethod
+#    def setUpClass(cls):
+#        super(SimpleTest, cls).setUpClass()
+#        django.setup()
+
+#    def test_basic_addition(self):
+#        """
+#        Tests that 1 + 1 always equals 2.
+#        """
+#        self.assertEqual(1 + 1, 2)
